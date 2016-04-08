36937c403da547014400fc463691f454dcb1e983
Jérôme Augé <jerome.auge@anakeen.com>
2016-04-08 11:01:41 +0200

    refactor(license): Changed license to GNU Lesser General Public License v3.0 (refs #6250)

--- po2js.php
+++ po2js.php
@@ -2,7 +2,6 @@
 /**
  * translate mo file to je file obejct
  * @author Anakeen
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  */
 
 Class po2js {
